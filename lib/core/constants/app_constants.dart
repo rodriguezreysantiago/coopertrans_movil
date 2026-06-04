338a736c2913af7684ea503569170567c6b67e29
rodriguezreysantiago <santiagocoopertrans@gmail.com>
2026-06-04 12:39:27 -0300

    chore: bump version 1.0.88+91

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -133,7 +133,7 @@ class AppTexts {
 
   static const String rutaNoEncontrada = 'Ruta no encontrada';
   // Podés mantener un registro visual de tu versión acá
-  static const String appVersion = 'v 1.0.87';
+  static const String appVersion = 'v 1.0.88';
 }
 
 // ===========================================================================
